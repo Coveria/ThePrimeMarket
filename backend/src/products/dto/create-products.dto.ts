2eef7aaeabc513f591a09f83a011c2e6965f6cfd
Bohdan Diatliuk <dyatlyuk.bohdan@gmail.com>
2026-03-22 00:14:43 +0200

    update data

--- backend/src/products/dto/create-products.dto.ts
+++ backend/src/products/dto/create-products.dto.ts
@@ -1,19 +1,90 @@
-import { IsNumber, IsPositive, IsString, MinLength } from 'class-validator';
+import {
+  IsArray,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class CreateProductDto {
   @IsString()
   @MinLength(2)
   title: string;
 
-  @IsString()
-  description: string;
+  @IsArray()
+  @IsString({ each: true })
+  description: string[];
 
   @IsNumber()
   @IsPositive()
   price: number;
 
+  @IsNumber()
+  @IsOptional()
+  discount?: number;
+
+  @IsArray()
+  @IsString({ each: true })
+  images: string[];
+
+  @IsNumber()
+  @IsOptional()
+  stock?: number;
+
   @IsString()
-  image: string;
+  @IsOptional()
+  connection?: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  features?: string[];
+
+  @IsString()
+  @IsOptional()
+  series?: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  interface?: string[];
+
+  @IsString()
+  @IsOptional()
+  power?: string;
+
+  @IsString()
+  @IsOptional()
+  batteryType?: string;
+
+  @IsString()
+  @IsOptional()
+  button?: string;
+
+  @IsString()
+  @IsOptional()
+  color?: string;
+
+  @IsString()
+  @IsOptional()
+  country?: string;
+
+  @IsString()
+  @IsOptional()
+  type?: string;
+
+  @IsString()
+  @IsOptional()
+  size?: string;
+
+  @IsString()
+  @IsOptional()
+  weight?: string;
+
+  @IsString()
+  @IsOptional()
+  design?: string;
 
   @IsNumber()
   categoryId: number;
